43db8c0175b343d47f72d98dd467e8e8ead2cdf7
David S. <116600844+drshooby@users.noreply.github.com>
2025-03-25 19:51:43 -0700

    removed dead code

--- setenv.py
+++ setenv.py
@@ -4,10 +4,6 @@ aws_access_key = input("Enter AWS Access Key ID: ")
 aws_secret_key = input("Enter AWS Secret Access Key: ")
 aws_session_token = input("Enter AWS Session Token (if applicable): ")
 
-os.environ["AWS_ACCESS_KEY_ID"] = aws_access_key
-os.environ["AWS_SECRET_ACCESS_KEY"] = aws_secret_key
-os.environ["AWS_SESSION_TOKEN"] = aws_session_token
-
 on_windows = input("Are you using Windows? (y/n)...type y or n otherwise assume no: ")
 print(f'\nRun the following command in your shell to set AWS credentials:')
 if on_windows.lower() == "y":
