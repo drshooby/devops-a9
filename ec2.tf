cf7c54bcf9006ca970c5508f3039b6ec7a39d49f
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-03-25 17:26:24 -0700

    add port rules per article

--- ec2.tf
+++ ec2.tf
@@ -12,6 +12,50 @@ resource "aws_security_group" "private_instances" {
       security_groups = [ module.bastion.security_group_id ]
   }
 
+  # For docker swarm
+  # TCP port 2377 for cluster management communications,
+  # TCP and UDP port 7946 for communication among nodes,
+  # TCP and UDP port 4789 for overlay network traffic.
+  ingress {
+    description = "Docker swarm"
+    from_port   = 2377
+    to_port     = 2377
+    protocol    = "tcp"
+    cidr_blocks = [ module.vpc.vpc_cidr_block ]
+  }
+
+  ingress {
+    description = "Docker swarm"
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "tcp"
+    cidr_blocks = [ module.vpc.vpc_cidr_block ]
+  }
+
+  ingress {
+    description = "Docker swarm"
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "udp"
+    cidr_blocks = [ module.vpc.vpc_cidr_block ]
+  }
+
+  ingress {
+    description = "Docker swarm"
+    from_port   = 4789
+    to_port     = 4789
+    protocol    = "tcp"
+    cidr_blocks = [ module.vpc.vpc_cidr_block ]
+  }
+
+  ingress {
+    description = "Docker swarm"
+    from_port   = 4789
+    to_port     = 4789
+    protocol    = "udp"
+    cidr_blocks = [ module.vpc.vpc_cidr_block ]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
